496f7c98649f4148d5bcd60506e4ec5414ae8d5b
Emile Christie <emile.christie.pereira@ccc.ufcg.edu.br>
2025-12-28 16:56:30 -0300

    feat: download funcionando

--- client/src/components/DownloadForm.tsx
+++ client/src/components/DownloadForm.tsx
@@ -1,17 +1,30 @@
 import { Box, Button, createListCollection, DownloadTrigger, Flex,  FormatByte,  Icon, Input, InputGroup, Portal, Presence, Select, Spinner, } from "@chakra-ui/react";
 import { AudioLines, CirclePlay, DownloadIcon, Tally1, } from "lucide-react";
 import { groupBy } from "es-toolkit"
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { LuDownload } from "react-icons/lu";
 
 const DownloadForm: React.FC = () => {
 
+    const ws = useRef<WebSocket | null>(null);
+
     const [url, setUrl] = useState("");
     const [videoIcon,setVideoIcon] = useState(true);
-    const [format, setFormat] = useState<string[]>(["video_mp4_480"])
+    const [selection, setSelection] = useState<string[]>(["video_mp4_480"])
+    const [format, setFormat] = useState<string>("mp4")
     const [downloadTriggered, setDownloadTriggered] = useState(false);
+    const [quality, setQuality] = useState("480");
+    const [progress, setProgress] = useState(0);
+    const [status, setStatus] = useState("");
+    const [fileName, setFileName] = useState<string | null>(null);
     const [pct, setPct] = useState(0);
-    const data = "video aqui"
+    const [downloadReady, setDownloadReady] = useState(false);
+    const [data, setData] = useState<{
+  file: Blob;
+  mimetype: string;
+  extension: string;
+  size: number;
+} | null>(null);
 
     const collection = createListCollection({
       items: [
@@ -20,11 +33,6 @@ const DownloadForm: React.FC = () => {
         { label: "MP4 (480p)", value: "video_mp4_480", category: "Video" },
         { label: "MP4 (360p)", value: "video_mp4_360", category: "Video" },
         { label: "MP3", value: "audio_mp3", category: "Audio" },
-        { label: "OGG", value: "audio_ogg", category: "Audio" },
-        { label: "WAV", value: "audio_wav", category: "Audio" },
-        { label: "FLAC", value: "audio_flac", category: "Audio" },
-        { label: "M4A", value: "audio_m4a", category: "Audio" },
-        { label: "WEBM", value: "audio_webm", category: "Audio" },
       ],
     })
 
@@ -34,15 +42,64 @@ const DownloadForm: React.FC = () => {
 
     const changeFormat = (e:string[]) => {
       //alert(e);
-      setFormat(e);
+      setFormat(e[0].split("_")[1]);
+      setQuality(e[0].split("_")[2]);
+      setSelection(e)
      // alert(e[0].split("_")[0])
      setVideoIcon(e[0].split("_")[0] === "video")
     }
 
     const download = () => {
+      setDownloadReady(false);
       setDownloadTriggered(true);
-    }
+      setProgress(0);
+      setStatus("Baixando...");
+      setFileName(null);
 
+      ws.current = new WebSocket("ws://localhost:3001");
+
+      ws.current.onopen = () => {
+        ws.current?.send(
+          JSON.stringify({
+            url,
+            format,
+            quality,
+          })
+        );
+      };
+
+      ws.current.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        if (data.progress !== undefined) {
+          setProgress(data.progress);
+        }
+
+        if (data.done) {
+          setStatus("Prontinho! Cheque sua pasta de downloads :3");
+          setFileName(data.fileName);
+          ws.current?.close();
+          setDownloadReady(true);
+        }
+
+        if (data.error) {
+          setStatus(data.error);
+          ws.current?.close();
+        }
+      };
+
+      ws.current.onerror = () => {
+        setStatus("Erro de conexão");
+      };
+    };
+
+    const saveFile = () => {
+      if (!fileName) return;
+
+      window.location.href = `http://localhost:3001/download/${encodeURIComponent(
+        fileName
+      )}`;
+    };
 
   return (
     <>
@@ -67,6 +124,7 @@ const DownloadForm: React.FC = () => {
             }>
           <Input 
             ml={4}
+            disabled={downloadTriggered && !downloadReady}
             value={url}
             onChange={(e) => {
               setUrl(e.currentTarget.value)
@@ -86,13 +144,13 @@ const DownloadForm: React.FC = () => {
         collection={collection} 
         size="sm" 
         width="220px" 
-        defaultValue={["mp4_480"]}
-        value={format}
+        defaultValue={["video_mp4_480"]}
+        value={selection}
         onValueChange={(e)=>changeFormat(e.value)}
         >
           <Select.HiddenSelect />
           <Select.Control>
-            <Select.Trigger disabled={downloadTriggered} cursor={downloadTriggered ? "forbidden":"pointer"} border={"none"}outline={"none"}>
+            <Select.Trigger disabled={downloadTriggered && !downloadReady} cursor={downloadTriggered && !downloadReady ? "forbidden":"pointer"} border={"none"}outline={"none"}>
               <Select.ValueText color={"#F1F5F8"} placeholder="Formato" />
             </Select.Trigger>
             <Select.IndicatorGroup>
@@ -120,7 +178,7 @@ const DownloadForm: React.FC = () => {
         </Flex>
       </Box>
       <Box position={"relative"}>
-        <Button disabled={downloadTriggered} h={"full"} onClick={()=>download()} px={5} pb={1} alignContent={"center"} className={`button-95 ${!downloadTriggered ? "enabled-button" : "" }`} role="button">
+        <Button disabled={downloadTriggered && !downloadReady} h={"full"} onClick={()=>download()} px={5} pb={1} alignContent={"center"} className={`button-95 ${!downloadTriggered && !downloadReady ? "enabled-button" : "" }`} role="button">
             <Icon>
               <DownloadIcon color={"#F1F5F8"}/>
             </Icon>
@@ -133,28 +191,23 @@ const DownloadForm: React.FC = () => {
     animationName={{ _open: "fade-in", _closed: "fade-out" }}
     animationDuration="slower"
     mt={6}>
-      <Box>
-          <Button cursor={"default"} className="button-105" w={"max"} bgColor={"#ac5990d5"} color={"#faeeffff"} title="Download em progresso">
-            <Spinner size={"md"} color="#faeeffff" borderWidth="5px" animationDuration="0.65s" />
-            Download em {pct == 0 ? "progresso" : pct+"%"}, aguarde...
-          </Button>
-          {
-            /*
-          <DownloadTrigger
-          className="button-95"
-            data={data}
-            fileName="titulo.extensao"
-            mimeType="video/mp4 ou audio/webm por exemplo"
-            asChild
-          >
-            <Button variant="outline">
-              <LuDownload /> Baixar o {videoIcon ? "vídeo" : "áudio"} (
-              <FormatByte value={data.length} unitDisplay="narrow" />)
-            </Button>
-          </DownloadTrigger>
-          */
-          }
-      </Box>
+
+      <Flex color={"#c4c3c4ff"} gap={4} alignItems={"center"}>
+        <p>{progress}%</p>
+        <progress
+          value={progress}
+          max={100}
+          style={{
+            width: 420,
+            height: 12,
+            borderRadius: 6,
+            overflow: 'hidden',
+            appearance: 'none',
+          }}
+        />
+        <p>{status}</p>
+      </Flex>
+
     </Presence>
     </>
   );
